522c26d3a16f835a74e1e08d4a0b4d577a88f038
Musaib <musaib1.mukri@gmail.com>
2017-03-08 11:48:03 +0530

    Check for Game Tie

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-//"use strict";
-
 function Gamer(gridSize, combination) {
 
 	this.gridSize = 3;
 	this.combination = [];
 	this.step = this.combination.length;
+	this.currentPlayer = null;
 	
 	this.grid = [];
 	for(var x=1; x<=this.gridSize; x++){
@@ -15,23 +14,30 @@ function Gamer(gridSize, combination) {
 	}
 
 	this.gameEnd = false;
+	if(this.gameEnd){ gameUI.finishGame(); }
 
 }
 
-Gamer.prototype.setValues = function(x,y){
+Gamer.prototype.setValues = function(x,y, player){
+
+	this.currentPlayer = player;
+	//console.log(this.currentPlayer);
+
 	if(this.grid[x][y].search(/true/i) === -1){
 		this.step++;
 		this.grid[x][y] = "("+ x + "," + y +")" + " : true" ;
 		this.combination.push(this.grid[x][y]);
 		if(this.step >= this.gridSize){
-			Gamer.prototype.checkValues(this.combination);
+			Gamer.prototype.checkValues(this.combination, this.currentPlayer);
 		}
 	}else{
 		return false;
 	}
 };
 
-Gamer.prototype.straights = function(strCut, axis, combo){
+Gamer.prototype.straights = function(strCut, axis, combo, player){
+
+	this.currentPlayer = player;
 
 	var playerCombo = [];
 	for(x=0; x<combo.length; x++){
@@ -50,28 +56,32 @@ Gamer.prototype.straights = function(strCut, axis, combo){
   } 
 
   if (counts.indexOf(3) !== -1){ 
-  	console.log('Won with a ' + axis +' Match');
-  	this.gameEnd = true;
+  	console.log(this.currentPlayer + ' Won with a ' + axis +' Match');
+  	gameUI.finishGame(this.currentPlayer);
   }
 
 };
 
-Gamer.prototype.diagonals = function(combo){
+Gamer.prototype.diagonals = function(combo, player){
+
+	this.currentPlayer = player;
 
 	var xAxis, yAxis, same = 0;
 	for(x=0; x<combo.length; x++){
-		xAxis = m.combination[x].substr(1,1);
-		yAxis = m.combination[x].substr(3,1);
+		xAxis = combo[x].substr(1,1);
+		yAxis = combo[x].substr(3,1);
 		if(xAxis === yAxis){ same++; }
 	}
 	if(same === 3){
-		console.log('Won with a diagonal Match');
-  	this.gameEnd = true;
+		console.log(this.currentPlayer + ' Won with a diagonal Match');
+  	gameUI.finishGame(this.currentPlayer);
 	}
 
 };
 
-Gamer.prototype.antiDiagonals = function(combo){
+Gamer.prototype.antiDiagonals = function(combo, player){
+
+	this.currentPlayer = player;
 
 	var antiD = {}, AxAxis = [], AyAxis = [], same = 0;
 
@@ -90,26 +100,34 @@ Gamer.prototype.antiDiagonals = function(combo){
 		AyAxis[x] = antiD.yAxis[x];
 	}
 
+	//console.log(combo);
+
 	for(x=0; x<combo.length; x++){
-		xAxis = m.combination[x].substr(1,1);
-		yAxis = m.combination[x].substr(3,1);
-		if(parseInt(xAxis) === AxAxis[x] && parseInt(yAxis) === AyAxis[x]){
+		console.log(x);
+		antiD.xAxis[x] = combo[x].substr(1,1);
+		antiD.yAxis[x] = combo[x].substr(3,1);
+		if( (parseInt(antiD.xAxis[x]) === AxAxis[x] && parseInt(antiD.yAxis[x]) === AyAxis[x]) || 
+				(parseInt(antiD.xAxis[x]) === AyAxis[x] && parseInt(antiD.yAxis[x]) === AxAxis[x])
+			) {
 			same++;
+			//console.log(same);
 		}
 	}
 	
+	console.log(antiD.xAxis, AxAxis, antiD.yAxis, AyAxis);
+	
 	if(same === 3){
-		console.log('Won with a Anti diagonal Match');
-  	this.gameEnd = true;
+		console.log(this.currentPlayer + ' Won with a Anti diagonal Match');
+  	gameUI.finishGame(Gamer.currentPlayer);
 	}
 
 };
 
-Gamer.prototype.checkValues = function(combo){
-	Gamer.prototype.straights(3,'Horizontal', combo);
-	Gamer.prototype.straights(1,'Vertical', combo);
-	Gamer.prototype.diagonals(combo);
-	Gamer.prototype.antiDiagonals(combo);
+Gamer.prototype.checkValues = function(combo, player){
+	Gamer.prototype.straights(3,'Vertical', combo, player);
+	Gamer.prototype.straights(1,'Horizontal', combo, player);
+	Gamer.prototype.diagonals(combo, player);
+	Gamer.prototype.antiDiagonals(combo, player);
 };
 
 var player1 = new Gamer();
--- js/gameUI.js
+++ js/gameUI.js
@@ -7,7 +7,7 @@ var gameUI = {
 
 	startGame: function() {
 		$(this.screenFinish).hide(); $(this.screenBoard).hide(); $(this.screenStart).show();
-		$('header a.button').click(function(){ gameUI.newGame(); });  
+		$('header a.button').click(function(){ gameUI.newGame(); });
 	},
 
 	switchPlayerTurns: function(){
@@ -24,32 +24,63 @@ var gameUI = {
 	newGame: function() {
 		$(this.screenFinish).hide(); $(this.screenStart).hide(); $(this.screenBoard).show();
 		gameUI.switchPlayerTurns();
+		gameUI.gameReset();
 		$(this.box).click(function(e){ gameUI.gameStep(e); });
 	},
 
 	gameStep: function(e){
 		$this = $(e.target);
-		var boxNo = $this.attr('class');
+		var boxNoX = parseInt($this.attr('class').substr(11,1));
+		var boxNoY = parseInt($this.attr('class').substr(12,1));
 
 		if(gameUI.currentPlayerO){ 
-			$this.removeClass('empty').addClass('box-filled-1');
-			$('.players').eq(0).removeClass('active').end().eq(1).addClass('active'); 
-			player1.setValues(1,2);
-			gameUI.switchPlayerTurns();
+			if($this.hasClass('empty')){
+				$this.removeClass('empty').addClass('box-filled-1');
+				$('.players').eq(0).removeClass('active').end().eq(1).addClass('active'); 
+				player1.setValues(boxNoX, boxNoY, 'O');
+				gameUI.switchPlayerTurns();
+			}
 		}else{ 
-			$this.removeClass('empty').addClass('box-filled-2');
-			$('.players').eq(1).removeClass('active').end().eq(0).addClass('active'); 
-			player1.setValues(2,1);
-			gameUI.switchPlayerTurns();
+			if($this.hasClass('empty')){
+				$this.removeClass('empty').addClass('box-filled-2');
+				$('.players').eq(1).removeClass('active').end().eq(0).addClass('active'); 
+				player2.setValues(boxNoX, boxNoY, 'X');
+				gameUI.switchPlayerTurns();
+			}
 		}
+
+		if($('.boxes').find('.empty').length === 0){
+			gameUI.matchDraw();
+		}
+
 	},
 
-	finishGame: function() {
-		$(this.screenStart).hide(); $(this.screenBoard).hide(); $(this.screenFinish).show(); 
+	gameReset: function(e){
+
+		$('.boxes').find('li').removeClass('box-filled-1').addClass('empty');
+		$('.boxes').find('li').removeClass('box-filled-2').addClass('empty');
+
 	},
 
-	showWinner: function(){
-		console.log('Musaib');
+	finishGame: function(player) {
+		$(this.screenStart).hide(); $(this.screenBoard).hide(); $(this.screenFinish).show();
+		if(player === 'O'){
+			$(this.screenFinish).addClass('winnerO');
+		}else if(player === 'X'){
+			$(this.screenFinish).addClass('winnerX');
+		}
+		$('#finish a.button').click(function(){ gameUI.startGame(); });
+	},
+
+	matchDraw: function(){
+		$(this.screenStart).hide(); $(this.screenBoard).hide(); $(this.screenFinish).show();
+		$('#finish').find('p').html('Its a Tie!');
+	},
+
+	nextGame: function(){
+		gameUI.startGame();
+		gameUI.switchPlayerTurns();
+		gameUI.gameReset();
 	}
 
 };
